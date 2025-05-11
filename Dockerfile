afe10ecb326a60e75023e54566809ee215127ca1
WillFritsch <74694657+WillFritsch@users.noreply.github.com>
2025-05-11 03:36:48 +0000

    added docker pdo pgsql

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,6 @@ FROM mcr.microsoft.com/vscode/devcontainers/php:8-bullseye
 
 # update apt
 RUN apt update
+RUN apt-get install -y libpq-dev \
+    && docker-php-ext-configure pgsql -with-pgsql=/usr/local/pgsql \
+    && docker-php-ext-install pdo pdo_pgsql pgsql
--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+//require 'router.php';
+
+// connect to the database
+$dsn = 'pgsql:host=localhost;dbname=db; port=5432; user=user; password=applesauce; charset=utf8mb4';
+$pdo = new PDO($dsn);
+
+$statemen = $pdo->prepare("select * from posts");
+$statement->execute();
+
+$posts = $statement->fetchAll();
+
+dd($posts);
