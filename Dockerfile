1fa618e6e6000ed099d82bcc8cec8ab6497baa5e
WillFritsch <74694657+WillFritsch@users.noreply.github.com>
2025-05-12 03:40:21 +0000

    remove php 8

--- Dockerfile
+++ Dockerfile
@@ -9,5 +9,8 @@ RUN add-apt-repository ppa:ondrej/php
 
 RUN apt update
 
+RUN apt remove php8.0 -y
+
 RUN apt install php8.4 -y
+
 RUN apt install php8.4-common php8.4-cli php8.4-opcache php8.4-pgsql php8.4-xml php8.4-curl php8.4-zip php8.4-mbstring php8.4-gd php8.4-intl php8.4-bcmath -y
--- index.php
+++ index.php
@@ -4,12 +4,14 @@ require 'functions.php';
 //require 'router.php';
 
 //connect to the database
-$dsn = 'pgsql:host=localhost;dbname=db;port=5432;user=user;password=applesauce';
-$pdo = new PDO($dsn);
+//$dsn = 'pgsql:host=localhost;dbname=db;port=5432;user=user;password=applesauce';
+//$pdo = new PDO($dsn);
 
-$statement = $pdo->prepare("select * from posts");
-$statement->execute();
+//$statement = $pdo->prepare("select * from posts");
+//$statement->execute();
 
-$posts = $statement->fetchAll();
+//$posts = $statement->fetchAll();
 
-dd($posts);
+//dd($posts);
+
+phpinfo();
